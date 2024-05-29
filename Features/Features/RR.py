756c26fcf26bc7706039bb5ac478b42335e8cdb5
SjdTl <terlouwsjoerd@outlook.com>
2024-05-29 17:24:44 +0200

    rr

--- Features/Features/RR.py
+++ Features/Features/RR.py
@@ -54,7 +54,7 @@ def RR(unprocessed_ecg, fs=700):
     rr = normalize(ECG_to_RR(ecg, fs=fs))
 
     df_general = feat_gen.basic_features(rr, "RR")
-    df_specific = rr_specific_features(rr, fs)
+    df_specific = rr_peak_features(rr, fs)
 
     features = pd.concat([df_specific, df_general], axis=1)
 
@@ -74,70 +74,44 @@ def ECG_to_RR(ecg, fs=100, method = "vangent2019"):
 
         return edr
     
-def rr_specific_features(rr, fs=700):
+def rr_peak_features(rr, fs=700,method="Neurokit"):
     """
     Description
     -----------
-    Calculate features specific to RR signal
+    Calculate peak features of RR signal
     
     Parameters
     ----------
     rr : np.array
         processed rr signal
     fs : float or int
-        sampling frequency of ecg signal
+        sampling frequency of rr signal
     
     Returns
     -------
     out : pd.DataFrame
         dataframe containing the features mentioned in the docstring of RR()
-    
-    Raises
-    ------
-    error
-         description
-    
-    Notes
-    -----
-    
-    Examples
-    --------
-    >>>
     """
+
+    peak_index, through_index = peak_detection_RR(rr, fs=fs, method=method)
 
     # Find features
     out_dict = {}
-    
+    T = np.size(rr) / fs
+
+    out_dict["Respitory rate"] = np.size(peak_index) / T * 60
+
     # Turn dictionary into pd.DataFrame and return
     return pd.DataFrame.from_dict(out_dict, orient="index").T.add_prefix("RR_")
 
-def test(filepath):
-    """
-    Description
-    -----------
-    Function to test the signal, without having to call the entire database. Please use this function when looking for data to plot for the report.
+def peak_detection_RR(rr, fs=700, method = "Neurokit"):
     
-    Parameters
-    ----------
-    Filepath: string
-        Filepath to the test signal. This should be a pickled dictionary with the following format:
-            dict = {EDA: [..]
-                    EMG: [..]
-                    ECG: [..]}
-        Each signal is of one person, one label and includes only a small timeframe
-    Returns
-    -------
-    df: pd.DataFrame
-        Dataframe containing the features 
-        
-    """
-    ecg = feat_gen.load_test_data("ECG", filepath)
+    if method == "Neurokit":
+        info, data = nk.rsp_peaks(rr, sampling_rate = fs)
+        peak_index = (data["RSP_Peaks"])
+        through_index = (data["RSP_Troughs"])
 
-    feat_gen.quick_plot(ecg, preProcessing(ecg, 700), normalize(ECG_to_RR(preProcessing(ecg,700))))
-    # rpeak_detector(preProcessing(ecg), 700)
-
-    df = RR(ecg, 700)
-    return df
+    return peak_index, through_index
 
 # dir_path = os.path.dirname(os.path.realpath(__file__))
 # filepath = os.path.join(dir_path, "Raw_data", "raw_small_test_data.pkl")
--- Features/RR_test.py
+++ Features/RR_test.py
@@ -9,14 +9,17 @@ import matplotlib.pyplot as plt
 import tqdm
 from sklearn.preprocessing import minmax_scale as normalize
 import neurokit2 as nk
+from scipy.fft import fft, fftfreq, fftshift
+from scipy.signal import freqz
 
 from Features import ECG
 from Features import RR
 from Features.feat_head import split_time
+from Features import feat_gen
 
 dirpath = (os.path.dirname(os.path.realpath(__file__)))
 
-def spider_data(folderpath):
+def spider_data(folderpath, testing = False, T=60, fs= 100):
     """
     Description
     -----------
@@ -35,44 +38,60 @@ def spider_data(folderpath):
                 - Column 1: ecg in mV (value range from -1.5mV to 1.5mV)
                 - Column 2: timestamp with format hhmmss.milliseconds, used for the mapping of the video clip time windows (can be ignored)
                 - Column 3: can be ignored
+    testing : boolean
+        Only return a small part of the data, don't return everything
+    T : int
+        Length of the small array
+        
 
     
     Returns
     -------
+    IF TESTING IS FALSE
     data : dictionary of np.arrays
         dictionary of the form:
             data = {RR = [the rreathing data] 
                     ECG = [the ecg data]}
-    
-    Notes
-    -----
 
-    Examples
-    --------
-    >>>
+    IF TESTING IS TRUE
+    ecg : np.array
+        ecg data of length T
+    rr : np.array
+        rr data of length T 
     """
 
 
     rr = np.array([])
     ecg = np.array([])
 
+    i=0
     for root, dirs, files in os.walk(folderpath, topdown=False):
         for name in tqdm.tqdm(dirs):
             cur_rr = np.genfromtxt(os.path.join(folderpath, name, "Bitalinobr.txt"), delimiter="")[:,0]
             cur_ecg = np.genfromtxt(os.path.join(folderpath, name, "BitalinoECG.txt"), delimiter="")[:,0]
             rr = np.concatenate((rr, cur_rr))
             ecg = np.concatenate((ecg, cur_ecg))
+            i = i+1
+            if testing == True and i==3:
+                break
+                
 
     data = {"RR": rr,
             "ECG": ecg}
     
-    return data
+    if testing == True:
+        ecg_splitted = split_time(np.array([data["ECG"]]), fs, T)[0]
+        rr_splitted = split_time(np.array([data["RR"]]), fs, T)[0]
+        random_index = np.random.randint(low=0, high = ecg_splitted.shape[0]-1, size=1)
+        return ecg_splitted[random_index][0], rr_splitted[random_index][0]
+    else: 
+        return data
 
-def preProcess(rr, fs=100):
+def preProcessRR(rr, fs=100):
     """
     Description
     -----------
-    Filter the rreathing signal using a highpass and a lowpass filter
+    Filter the breathing signal using a highpass and a lowpass filter
     Most rreathing will always happen between 4-60 rreaths per minute.
     See also Peter H Charlton et al 2017 Physiol. Meas. 38 669, Chapter 3.6
     This corresponds to 4/60 and 60/60 rreath/s or Hz, since rreathing follows a sinusoidal pattern
@@ -88,36 +107,34 @@ def preProcess(rr, fs=100):
     -------
     RR_hl : np.array
          high- and lowpassed respitory rate signal. 
-    
-    Notes
-    -----
-    
     """
+
     order=5
 
     # highpass filter
-    lowcut= 4 #rreaths/min
-    lowcut = lowcut/(60) #Hz (does not have to be normalized since fs is specified in butter())
+    lowcut= 4 #breaths/min
+    lowcut = lowcut/(60) #Hz
     b, a = butter(5, lowcut, btype = 'highpass', fs=fs)
     rr_h = lfilter(b,a,rr)
 
     # lowpass filter
-    highcut=60 # rreaths/min
-    highcut= highcut/(60) # Hz (does not have to be normalized since fs is specified in butter())
+    highcut=60 # breaths/min
+    highcut= highcut/(60) # Hz 
     b, a = butter(5, highcut, btype="lowpass", fs=fs)
     rr_hl = lfilter(b,a,rr_h)
     return rr_hl
 
-from scipy.fft import fft, fftfreq
-def fft_RR(rr, fs=100):
-    yf = fft(rr)
-    xf = fftfreq(rr.size, 1/fs)
 
-    plt.plot(xf, np.abs(yf))
-    plt.show()
-
-
-def plot_spider(ecg, rr, rr_extracted, rr_unprocessed, shift, CC, method, fs=100):
+def plot_spider(ecg, rr, rr_extracted, rr_unprocessed, method, fs=100):
+    """
+    Description
+    -----------
+    One plot of subplots of:
+        - ECG data
+        - Respitory rate (processed and preprocessed)
+        - Extracted respitory rate using method method
+    """
+ 
     # normalize
     rr_extracted = normalize(rr_extracted)
     rr_unprocessed = normalize(rr_unprocessed)
@@ -144,76 +161,15 @@ def plot_spider(ecg, rr, rr_extracted, rr_unprocessed, shift, CC, method, fs=100
     ax[2].set_title("Processed RR")
     ax[2].set_ylabel("RR")
 
-    rr_ex_shift = np.concatenate((np.zeros(int(shift*fs)), rr_extracted[int(shift * fs):]))
-    ax[3].plot(t, rr_ex_shift)
+    ax[3].plot(t, rr_extracted)
     ax[3].set_xlabel("Time ($s$)")
     ax[3].set_title("Extracted RR")
     ax[3].set_ylabel("RR")
 
-    plt.suptitle(f"Respiration rate extracted from ecg with an accuracy of {round(CC,2)}")
+    plt.suptitle(f"Respiration rate extracted from ecg")
 
     plt.tight_layout()
     plt.savefig(os.path.join(dirpath, "Plots", "RR_plots", "RR_"+str(method) + ".svg"))
-
-def compare_extracted_vs_real(RR_extracted, RR_real, step = 0.05, max_shift = 1, fs=100):
-    """
-    Description
-    -----------
-    Determine the accuracy of the extraction method using the Pearson Correlation Coefficient (https://en.wikipedia.org/wiki/Pearson_correlation_coefficient)
-    Also used in Peter H Charlton et al 2017 Physiol. Meas. 38 669, Chapter 3.6
-
-    - Normalize both signals
-    - Start by removing the phase shift between the signals (since this is irrelevant for accuracy)
-    - Determine CC using numpy, 
-        as defined $\rho_{X,Y}=\frac{\operatorname{cov}(X,Y)}{\sigma_X \sigma_Y}$ or in ASCII: Px,y = cov(X,Y)/(Sx * Sy)
-
-    Parameters
-    ----------
-    RR_extracted : np.array
-        Array consisting of the respitory rate extracted from the ECG data
-    RR_real : np.array
-        Array consisting of the respitory rate as measured (and preprocessed of course)
-    step : float
-        Resolution (in seconds) for calculating the shift
-    max_shift : float
-        Maximum seconds of phase shift
-    fs : int
-        Sampling rate 
-    
-    Intermediate 
-    ------------
-    CC_matrix : np.array
-        Covariance matrix https://en.wikipedia.org/wiki/Covariance_matrix of the form:
-        
-        cov(*X*) = [1, Pxy 
-                  Pxy, 1]
-        
-    Returns
-    -------
-    CC : float
-        Pearson's correlation coefficent (-1<p<1)
-    shift : float
-        Phase shift in seconds between the two signals
-    """
-
-    # Normalize 
-    RR_extracted = normalize(RR_extracted)
-    RR_real = normalize(RR_real)
-    CC = []
-    shift = []
-    # Calculate CC for all phase shifts -2s till 2s with steps of 0.05s
-    # Move a standard smaller window of RR_real (called RR_window) alongside RR_extracted (RR_cur)
-    RR_window = RR_real[int(fs * max_shift):int(-fs * max_shift)]
-
-    timeshifts = np.arange(0,2 * max_shift,step) #s
-    for current_shift in timeshifts:
-        RR_cur = RR_extracted[int(current_shift*fs):-(int(2 * max_shift*fs)-int(current_shift*fs))] # written like this to prevent rounding errors
-        CC_matrix =np.corrcoef(RR_window, RR_cur)
-        CC.append(CC_matrix[0][1])
-        shift.append(current_shift)
-
-    max_index = np.argmax(CC)
-    return CC[max_index], shift[max_index] - max_shift
 
 
 def determine_RR_accuracy(dataset, method="vangent2019", T=60, example = True):
@@ -248,87 +204,194 @@ def determine_RR_accuracy(dataset, method="vangent2019", T=60, example = True):
         array of the correlation coefficients of all the windows
     shift : np.array
         array of the timeshifts of all the windows
-    
-    Raises
-    ------
-    error
-         description
-    
-    Notes
-    -----
-    
-    Examples
-    --------
-    >>>
     """
-    fs = 100 # for spider fearful dataset
+    1==1
+    # fs = 100 # for spider fearful dataset
 
-    # Split up data
-    RR_split = split_time(np.array([dataset["RR"]]), fs, T)[0]
-    ECG_split = split_time(np.array([dataset["ECG"]]), fs, T)[0]
+    # # Split up data
+    # RR_split = split_time(np.array([dataset["RR"]]), fs, T)[0]
+    # ECG_split = split_time(np.array([dataset["ECG"]]), fs, T)[0]
 
-    # For selecting a window to plot
-    randomvalue = np.random.randint(low=0, high=RR_split.shape[0], size=1)
-    i = 0
+    # # For selecting a window to plot
+    # randomvalue = np.random.randint(low=0, high=RR_split.shape[0], size=1)
+    # i = 0
 
-    max_shift = 1
-    CC = []
-    shift = []
-    for rr, ecg in (zip(RR_split, ECG_split)):
-        processed_RR = preProcess(rr, fs)
-        ecg = nk.ecg_clean(ecg, sampling_rate=fs)
-        if method != "control":
-            extracted_RR = RR.ECG_to_RR(ecg, fs=fs, method = method)
-        if method == "control":
-            extracted_RR = processed_RR + np.sin(2*np.pi * np.linspace(0, T/2, processed_RR.size))
+    # max_shift = 1
+    # CC = []
+    # shift = []
+    # for rr, ecg in (zip(RR_split, ECG_split)):
+    #     processed_RR = preProcessRR(rr, fs)
+    #     ecg = nk.ecg_clean(ecg, sampling_rate=fs)
+    #     if method != "control":
+    #         extracted_RR = RR.ECG_to_RR(ecg, fs=fs, method = method)
+    #     if method == "control":
+    #         extracted_RR = processed_RR + np.sin(2*np.pi * np.linspace(0, T/2, processed_RR.size))
 
-        cur_CC, cur_shift = compare_extracted_vs_real(extracted_RR, processed_RR, max_shift=max_shift)
-        CC.append(cur_CC)
-        shift.append(cur_shift)
+    #     cur_CC, cur_shift = compare_extracted_vs_real(extracted_RR, processed_RR, max_shift=max_shift)
+    #     CC.append(cur_CC)
+    #     shift.append(cur_shift)
 
-        if example == True and i==randomvalue:
-            plot_spider(ecg, processed_RR, extracted_RR, rr, cur_shift+max_shift, cur_CC, method)
-        i += 1
+    #     if example == True and i==randomvalue:
+    #         plot_spider(ecg, processed_RR, extracted_RR, rr, cur_shift+max_shift, cur_CC, method)
+    #     i += 1
 
-    return CC, shift
+    # return CC, shift
     
 
-def compare_methods(dataset, methods=["control", "vangent2019", "soni2019", "charlton2016", "sarkar2015"], T=60, example = True):
-    all_CC = []
-    all_shift = []
+def compare_methods(methods=["Original", "vangent2019", "soni2019", "charlton2016", "sarkar2015"], T=60):
+    rr = preProcessRR(rr)
+    rr_extracted = 
 
-    for method in methods:
-        cur_CC, cur_shift = determine_RR_accuracy(dataset, method, T, example)
-        all_CC.append(cur_CC)
-        all_shift.append(cur_shift)
+    features = {}
 
-    fig, ax = plt.subplots(1,2)
-    ax[0].boxplot(all_CC, labels=methods)
-    ax[0].set_ylabel("CC")
-    ax[1].boxplot(all_shift, labels=methods)
-    ax[1].set_ylabel("Time (s)")
-    ax[0].set_title("CC of RR extraction")
-    ax[1].set_title("Timeshift")
-    ax[0].set_xticklabels(methods, rotation=60)
-    ax[1].set_xticklabels(methods, rotation=60)
-    plt.tight_layout()
-    plt.savefig(os.path.join(dirpath, "Plots", "RR_plots", "RR_method_comparison.svg"))
+    features["Original"] = 
+    # all_CC = []
+    # all_shift = []
+
+    # for method in methods:
+    #     cur_CC, cur_shift = determine_RR_accuracy(dataset, method, T, example)
+    #     all_CC.append(cur_CC)
+    #     all_shift.append(cur_shift)
+
+    # # PLOT
+    # fig, ax = plt.subplots(1,2)
+    # ax[0].boxplot(all_CC, labels=methods)
+    # ax[0].set_ylabel("CC")
+    # ax[1].boxplot(all_shift, labels=methods)
+    # ax[1].set_ylabel("Time (s)")
+    # ax[0].set_title("CC of RR extraction")
+    # ax[1].set_title("Timeshift")
+    # ax[0].set_xticklabels(methods, rotation=60)
+    # ax[1].set_xticklabels(methods, rotation=60)
+    # plt.tight_layout()
+    # plt.savefig(os.path.join(dirpath, "Plots", "RR_plots", "RR_method_comparison.svg"))
+    1==1
+
+
+def testRR(fs=100):
+    """
+    Description
+    -----------
+    Function to test the feature extraction
+    
+    Parameters
+    ----------
+
+    Returns
+    -------
+    df: pd.DataFrame
+        Dataframe containing the features 
+        
+    """
+    ecg, rr = spider_data(os.path.join(dirpath, "spiderfearful"), testing = True)
+    # feat_gen.quick_plot(rr, preProcessRR(rr))
+
+    rr = preProcessRR(rr)
+
+    # signals, info = nk.rsp_process(rr, sampling_rate=fs)
+    # feat_gen.quick_plot(signals["RSP_Raw"], signals["RSP_Clean"], signals["RSP_Amplitude"])
+    # feat_gen.quick_plot(signals["RSP_Clean"], signals["RSP_Rate"], signals["RSP_RVT"])
+    # feat_gen.quick_plot(signals["RSP_Clean"], signals["RSP_Symmetry_PeakTrough"], signals["RSP_Symmetry_RiseDecay"])
+    # feat_gen.quick_plot(signals["RSP_Clean"], signals["RSP_Peaks"], signals["RSP_Troughs"])
+
+    df = RR.rr_peak_features(rr, 100)
+    return df
+
+def testECG(ecg):
+    """
+    Description
+    -----------
+    Same as testRR, except that the RR extraction from ECG is also tested
+    """
+
+def RR_figures(T =40, fs = 100):
+    """
+    Plots used for the processing flowchart in chapter four
+    Returns x plots:
+        - Depends on how the preprocessing is done
+    """
+    ecg, rr = spider_data(os.path.join(dir_path, "spiderfearful"), testing = True, T=T)
+
+    # -----------------------------------------------
+    # UNPROCESSED
+    # -----------------------------------------------
+    # RR unprocessed timedomain
+    fig, ax = plt.subplots()
+
+    ax.plot(np.linspace(0,T, fs*T), rr)
+    ax.set_xlabel("Time [$s$]")
+    ax.set_ylabel("?")
+
+    fig.savefig(os.path.join(dir_path, "plots", "RR_plots", "rr_unprocessed_td.svg"))
+
+
+    # RR unprocessed frequency domain
+    yf = fftshift(fft(rr))
+    xf = fftshift(fftfreq(rr.size, d= 1/fs))
+
+    fig, ax = plt.subplots()
+
+    ax.plot(xf, 10 * np.log10(np.abs(yf)))
+    ax.set_xlabel("Frequency [$Hz$]")
+    ax.set_ylabel("Amplitude [dB]")
+    ax.set_ylim(ymin = -25)
+
+    fig.savefig(os.path.join(dir_path, "plots", "RR_plots", "rr_unprocessed_fd.svg"))
+
+    # -----------------------------------------------
+    # PROCESSED
+    # -----------------------------------------------
+    # RR processed timedomain
+    rr = preProcessRR(rr)
+
+    fig, ax = plt.subplots()
+
+    ax.plot(np.linspace(0,T, fs*T), rr)
+    ax.set_xlabel("Time [$s$]")
+    ax.set_ylabel("?")
+
+    fig.savefig(os.path.join(dir_path, "plots", "RR_plots", "rr_processed_td.svg"))
+
+
+    # RR processed frequency domain
+    yf = fftshift(fft(rr))
+    xf = fftshift(fftfreq(rr.size, d= 1/fs))
+
+    fig, ax = plt.subplots()
+
+    ax.plot(xf, 10 * np.log10(np.abs(yf)))
+    ax.set_xlabel("Frequency [$Hz$]")
+    ax.set_ylabel("Amplitude [dB]")
+    ax.set_ylim(ymin = -25)
+
+    fig.savefig(os.path.join(dir_path, "plots", "RR_plots", "rr_processed_fd.svg"))
+
+    # -----------------------------------------------
+    # Peak detection
+    # -----------------------------------------------
+    # RR peak detection timedomain
+    fig, ax = plt.subplots()
+
+    peak_index, through_index = RR.peak_detection_RR(rr, fs, method = "Neurokit")
+
+    t = np.linspace(0,T, fs*T)
+    ax.plot(t, rr, label = "RR signal")
+    ax.plot(t[peak_index], rr[peak_index], 'o', label="Peaks")
+    ax.plot(t[through_index], rr[through_index], 'o', label="Throughs")
+    ax.set_xlabel("Time [$s$]")
+    ax.set_ylabel("?")
+
+    fig.savefig(os.path.join(dir_path, "plots", "RR_plots", "rr_peak_detection_td.svg"))
 
 
 
-data = spider_data(os.path.join(dirpath, "spiderfearful"))
-compare_methods(data)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print(testRR())
+# RR_figures()
 
 
-# RR = d["RR"][0:2500]
-# RR_pre = preProcess(RR)
-# # fft_RR(RR)
-# # fft_RR(RR_pre)
+filepath = os.path.join(dir_path, "Raw_data", "raw_data.pkl")
+# print(test(filepath))
 
-# edr = RR.ECG_to_RR(d["ECG"][0:2500])
-
-# CC, shift = compare_extracted_vs_real(RR_pre, edr)
-# print(CC, shift)
-
-# plot_spider(ecg = (d["ECG"][0:2500]), rr = RR_pre/RR_pre.max(), rr_extracted=edr/edr.max(), rr_unprocessed=RR/RR.max(), fs=100, shift = shift)
+# compare_methods(data)
 
